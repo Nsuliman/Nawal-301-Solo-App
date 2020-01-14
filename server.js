1efe28237feaff580737fc821febecbfcbcc4bdc
Nsuliman <nsuliman88@gmail.com>
2020-01-14 05:15:31 +0200

    POL completed

---
+++ server.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/********************************************** dependencies ****************************************************/
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const superagent = require('superagent');
+const pg = require('pg');
+const methodOverride = require('method-override')
+const expressLayouts = require('express-ejs-layouts')
+
+// PORT 
+const PORT = process.env.PORT || 8822 ;
+
+// Allows 
+const server = express();
+server.use(cors());
+
+// DataBase
+const client = new pg.Client(process.env.DB_URL);
+client.on('error', err => console.error(err));
+
+server.use(express.json());
+server.use(express.static('./public'));
+server.use(express.urlencoded({ extended: true }));
+server.set('view engine', 'ejs');
+// server.use(methodOverride(middleware));
+
+/***************************************************** Routes  ****************************************************/
+
+// Proof Of Life 
+server.get('/pol', (req, res) => {
+    res.render('pages/pol');
+});
+
+
+
+/**************************************************** Server Listening  ********************************************/
+server.listen(PORT, () => console.log(`Nawal Solo App , listening On port # : ${PORT}`));
+  
