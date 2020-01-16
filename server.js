69aa3835953f8c0abf251009464780dfb81dd212
Nsuliman <nsuliman88@gmail.com>
2020-01-16 21:59:03 +0200

    Used two API

--- server.js
+++ server.js
@@ -33,21 +33,58 @@ server.get('/pol', (req, res) => {
     res.render('pages/pol');
 });
 
+// ERRORS  
+server.get('/error', (request, response) => {
+    response.render('pages/error');
+});
+
 // Rendering New Search
 server.get('/new', (req, res) => {
     res.render('pages/searches/new');
 });
 
 
-const url = `http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIFT_API}&q=human`;
-superagent.get(url)
-    .then(data => {
-        let path = data.body.data;
-        path.map( element =>
-            {
-                console.log('Element  : ', element.images.downsized_large.url);
+// Shows the Results 
+server.post('/searches', (req, res) => {
+    
+    let keyword = req.body.search;
+
+    if (req.body.searchtype === 'sticker') {
+
+       var url = `api.giphy.com/v1/stickers/search?api_key=${process.env.GIFT_API}&q=${keyword}` ;
+        console.log(' sticker url : \n\n\n\n\n\n ', url);   
+    }
+    else if (req.body.searchtype === 'gif') {
+        var  url = `http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIFT_API}&q=${keyword}`;
+        console.log(' gif url : \n\n\n\n\n\n', url);   
+    }
+
+    superagent.get(url)
+        .then(data => {
+            // console.log('dataaaaaaaaaaaaaaaaaaaaaaaaaaaaa Boddddddddddddddy : ', data.body);
+            // console.log('dataaaaaaaaaaaaaaaaaaaaaaaaaaaaa Itemmmmmmmmmmmmmmmms : ', data.body.data);
+            let arr = data.body.data;
+            let gifts = arr.map(gift => {
+                console.log('gift : ', gift.images.downsized_large.url);
+                return new Gift(gift);
             });
-    });
+            res.render('pages/gifts/show', { gifts: gifts })
+        })
+        .catch(error => {
+            // console.log('Errorrrrrrrrrrrr : ', error);
+            res.render('pages/error');
+        });
+});
+
+
+function Gift(data) {
+
+    //The PIPE used if there's an empty data , to avoid error 
+    this.type = data.type;
+    this.title = data.title;
+    this.source = (data.source && data) || ' No Source Found';
+    this.image = (data.images.preview_gif.url) || ' No Image Found ';
+} // End of gift constructor function 
 
 
 /**************************************************** Server Listening  ********************************************/
